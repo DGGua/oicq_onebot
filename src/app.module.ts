a2249a2a3bddb9eacfe073315c964c252db04608
DG_Gua <DG_Gua@foxmail.com>
2022-10-07 20:23:54 +0800

    fix gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { WsStartGateway } from "./websocket/ws.gateway";
+import { WsGateway } from "./websocket/ws.gateway";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, WsStartGateway],
+  providers: [AppService, WsGateway],
 })
 export class AppModule {}
--- src/websocket/ws.gateway.ts
+++ src/websocket/ws.gateway.ts
@@ -1,13 +1,26 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
 } from "@nestjs/websockets";
-import * as WebSocket from "ws";
+import { WebSocket } from "ws";
 
 @WebSocketGateway(8081)
-export class WsStartGateway {
+export class WsGateway
+  implements OnGatewayConnection<WebSocket>, OnGatewayDisconnect<WebSocket>
+{
+  private clients: WebSocket[] = [];
+
+  handleConnection(client: WebSocket, ...args: any[]) {
+    this.clients.push(client);
+  }
+
+  handleDisconnect(client: WebSocket) {
+    this.clients.splice(this.clients.indexOf(client), 1);
+  }
   @SubscribeMessage("hello")
   hello(@MessageBody() data: any): any {
     return {
@@ -21,5 +34,11 @@ export class WsStartGateway {
     console.log("收到消息 client:", client);
     client.send(JSON.stringify({ event: "tmp", data: "这里是个临时信息" }));
     return { event: "hello2", data: data };
+  }
+  private broadcast(message: any) {
+    const broadCastMessage = JSON.stringify(message);
+    for (const c of this.clients) {
+      c.send(broadCastMessage);
+    }
   }
 }
